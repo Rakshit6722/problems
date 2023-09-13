a53c879045b990ce65e6eec37f10f83693ab020e
Rakshit6722 <rakshitchauhan435@gmail.com>
2023-09-13 14:56:44 +0530

    optimal approach rotate image

--- rotateImage.java
+++ rotateImage.java
@@ -4,21 +4,49 @@ public class rotateImage {
     public static void main(String[] args) {
         
     }
-    public static void rotate(int[][] matrix) {
-        int n = matrix.length;
-        int[][] ans = new int[matrix.length][matrix[0].length];
+    // public static void rotate(int[][] matrix) {
+    //     int n = matrix.length;
+    //     int[][] ans = new int[matrix.length][matrix[0].length];
 
-        //putting matrix elements in right position in new array
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                ans[j][n-1-i]=matrix[i][j];
+    //     //putting matrix elements in right position in new array
+    //     for(int i=0;i<matrix.length;i++){
+    //         for(int j=0;j<matrix[0].length;j++){
+    //             ans[j][n-1-i]=matrix[i][j];
+    //         }
+    //     }
+    //     //putting elmenets back in matrix array
+    //     for(int i=0;i<matrix.length;i++){
+    //         for(int j=0;j<matrix[0].length;j++){
+    //             matrix[i][j]=ans[i][j];
+    //         }
+    //     }
+    // }
+
+    //optimal appraoch
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+    
+        // Transpose the matrix (swap rows and columns)
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
-        //putting elmenets back in matrix array
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                matrix[i][j]=ans[i][j];
+    
+        // Reverse each row of the transposed matrix
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
             }
         }
     }
+    
 }
