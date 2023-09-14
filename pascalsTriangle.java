8fb89c777f1dc6788cba236d30c1a386a0435001
Rakshit6722 <rakshitchauhan435@gmail.com>
2023-09-14 13:53:41 +0530

    print nth row

--- pascalsTriangle.java
+++ pascalsTriangle.java
@@ -3,6 +3,7 @@ package problems;
 public class pascalsTriangle {
     public static void main(String[] args) {
         System.out.println(nthElement(4-1, 2-1 ));
+        nthRow(4);
     }
 
     //to find nth element in pascal triangle
@@ -13,5 +14,16 @@ public class pascalsTriangle {
             res = res/(i+1);
         }
         return res;
+    }
+
+    //to print nth row
+    static void nthRow(int n){
+        int ans = 1;
+        System.out.print(ans+" ");
+        for(int i = 1; i<n; i++){
+            ans = ans * (n-i);
+            ans /= i;
+            System.out.print(ans+" ");
+        }
     }
 }
