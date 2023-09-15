477b9328f5c3f339065e5b40b7706046b389d15e
Rakshit6722 <rakshitchauhan435@gmail.com>
2023-09-15 23:50:00 +0530

    optimal approach

--- threeSum.java
+++ threeSum.java
@@ -33,23 +33,61 @@ public class threeSum {
     // }
 
     // better approach
-    public static List<List<Integer>> threeSum(int[] nums) {
+    // public static List<List<Integer>> threeSum(int[] nums) {
 
-        Set<List<Integer>> ans = new HashSet<>(); // set which contains list of integer
+    // Set<List<Integer>> ans = new HashSet<>(); // set which contains list of
+    // integer
+
+    // for (int i = 0; i < nums.length; i++) {
+    // Set<Integer> st = new HashSet<>(); // hashset to store value of nums[j], with
+    // every i iteration new hashSet is created
+    // for (int j = i + 1; j < nums.length; j++) {
+    // int third = -(nums[i] + nums[j]); // nums[i]+nums[j]+nums[k] = 0 -> nums[k]
+    // or third = -(nums[i]+nums[j])
+    // if (st.contains(third)) { // if set contains third then we found a triplet
+    // List<Integer> temp = Arrays.asList(nums[i], nums[j], third); // store triplet
+    // in list
+    // temp.sort(null); // sort list
+    // ans.add(temp); // store list in set made earlier
+    // }
+    // st.add(nums[j]); // add nums[j] in hashset
+    // }
+    // }
+    // List<List<Integer>> res = new ArrayList(ans); // store ans in res List
+    // return res;
+    // }
+
+    // Optimal approach - two pointer approach
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList();
+
+        Arrays.sort(nums); //sort
 
         for (int i = 0; i < nums.length; i++) {
-            Set<Integer> st = new HashSet<>(); // hashset to store value of nums[j], with every i iteration new hashSet is created
-            for (int j = i + 1; j < nums.length; j++) {
-                int third = -(nums[i] + nums[j]); // nums[i]+nums[j]+nums[k] = 0 -> nums[k] or third = -(nums[i]+nums[j])
-                if (st.contains(third)) { // if set contains third then we found a triplet
-                    List<Integer> temp = Arrays.asList(nums[i], nums[j], third); // store triplet in list
-                    temp.sort(null); // sort list
-                    ans.add(temp); // store list in set made earlier
+            if (i > 0 && nums[i] == nums[i - 1]) //if nums[i] is equals to previous element then move i ahead
+                continue;
+
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum > 0) { 
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    //to check we are not taking the same elements for making triplets that we have used just now
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
                 }
-                st.add(nums[j]); // add nums[j] in hashset
             }
         }
-        List<List<Integer>> res = new ArrayList(ans); // store ans in res List
-        return res;
+        return ans;
     }
 }
