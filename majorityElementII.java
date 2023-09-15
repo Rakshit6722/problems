c9ef33d217251fdaa4891c4fcb02f7b75da1695d
Rakshit6722 <rakshitchauhan435@gmail.com>
2023-09-15 19:57:43 +0530

    brute force

---
+++ majorityElementII.java
@@ -0,0 +1,38 @@
+package problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class majorityElementII {
+    public static void main(String[] args) {
+        int[] arr = {3,2,3};
+        System.out.println(majorityElement(arr));
+    }
+
+    public static List<Integer> majorityElement(int[] nums) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        int n = nums.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (ans.size() == 0 || ans.get(0) != nums[i]) {
+                count = 0;
+                for (int j = 0; j < n; j++) {
+                    if (nums[i] == nums[j]) {
+                        count++;
+                    }
+                }
+                if (count > n / 3) {
+                    ans.add(nums[i]);
+
+                }
+
+            }
+            if (ans.size() == 2)
+                break;
+
+        }
+        return ans;
+    }
+
+}
